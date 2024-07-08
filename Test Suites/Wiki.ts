4d77e072340c629ad65011758592bfb64ab1f944
0725 <0725@DESKTOP-09GUU8E.mshome.net>
2024-07-08 10:15:10 +0700

    commit 3

--- Test Suites/Wiki.ts
+++ Test Suites/Wiki.ts
@@ -25,62 +25,6 @@
       </variableLink>
    </testCaseLink>
    <testCaseLink>
-      <guid>0ae6e2f2-2966-4c25-a73f-5e11f14471c2</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/Wiki/Cina</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>b13e1a3f-54db-45f5-89ff-b62914efa215</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/Wiki/Deustch</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>b4d5c2d7-1126-4bc3-9beb-5bbe875834da</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/Wiki/English</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>6fb1710a-aed8-425d-a491-6bf47ae45f81</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/Wiki/France</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>4b79336f-eb51-40d0-8c57-eb3412ba4b71</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/Wiki/Italia</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>f5d79dfe-3bde-434f-abcf-467ee689cabd</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/Wiki/Jepang</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>02ae4a8e-d042-4a7d-a30e-d5654e3940f8</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/Wiki/Portugese</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>0128e9e7-71d0-4a71-87dc-502ccfb9b1d5</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/Wiki/Rusia</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
       <guid>8e33cec2-6a57-4be7-84a6-4629afaed2a4</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
